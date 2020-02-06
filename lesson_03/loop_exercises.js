afec8b7171be72cb17a9d9fdb4c8c1864368c7a3
nhtaylor <nt334407@gmail.com>
2020-02-05 21:16:21 -0500

    exercise finishes

---
+++ lesson_03/loop_exercises.js
@@ -0,0 +1,40 @@
+// from website
+
+// let n = 0
+//
+// while (n < 5) {
+//   console.log(n)
+//   n++
+// }
+
+
+// my experience
+
+// let m = 10
+//
+// while (m > 0) {
+//   console.log(m)
+//   m--
+// }
+
+
+// do while statement
+
+// let n = 0
+//
+// do {
+//   console.log(n)
+//   n++
+// } while (n < 0)
+
+
+// for loop
+// for (let i = 0; i < 5; i++) {
+//   console.log(`${i} fish`)
+// }
+
+
+// for you exercise
+for (let i = 10; i > 0; i--) {
+  console.log(`${i} bottles of beer on the wall`)
+}
