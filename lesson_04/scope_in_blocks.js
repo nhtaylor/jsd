c88e2ba6ecc438da6a0f29711e57c52288b78f27
nhtaylor <nt334407@gmail.com>
2020-02-12 19:33:30 -0500

    lesson 4 practice iwth scope

---
+++ lesson_04/scope_in_blocks.js
@@ -0,0 +1,25 @@
+// example of code block creating local scope when variable is declared using let or const
+
+// scope in block statements
+// let name
+if (true) {
+    // this 'if' conditional block creates a new scope because we use 'let'
+   const name = 'Prince' // name is local to the if block
+  console.log(name) // logs 'Prince'
+}
+// let name
+console.log('----')
+console.log(name) // <--- prints nothing because the 'name' variable can only be accessed from within the block (due to the use of 'let')
+// hmm
+
+
+// example of code block NOT CREATING local scope when a variable is using local scope
+// let name
+if (true) {
+    // this 'if' conditional block creates a new scope because we use 'let'
+   const name = 'Prince' // name is local to the if block
+  console.log(name) // logs 'Prince'
+}
+// let name
+console.log('----')
+console.log(name) // <--- prints nothing because the 'name' variable can only be accessed from within the block (due to the use of 'let')
--- lesson_04/scope_practice.js
+++ lesson_04/scope_practice.js
@@ -1,33 +1,70 @@
-// example of global scope
-// Global function because it was declared outside of a function
-const name = 'Simone Biles'
+// // example of global scope
+// // Global function because it was declared outside of a function
+// const name = 'Simone Biles'
+//
+// console.log(name)
+// console.log('----')
+//
+// function bestGymnastEver() {
+//   // 'name' variable is accessible here and everywhere else because it is global
+//   console.log(name)
+// }
+//
+// bestGymnastEver() // logs 'Simone Biles'
+//
+//
+//
+// // example of local scope
+// function showInspirationalMessage() {
+//   // "message" variable is declared **inside** a function which means it is LOCAL to this function
+//
+//   const message = "Don't give up, you can do it!!"
+//   console.log(message)
+// }
+//
+// // prints message variable
+// showInspirationalMessage()
+//
+// /*
+// Now let's attempt to directly access the message variable
+// and log it out to the console
+// */
+//
+// console.log(message) // <-- Error! The variable is local to the function can cannot be "directly" accessed outside of the function
 
-console.log(name)
-console.log('----')
 
-function bestGymnastEver() {
-  // 'name' variable is accessible here and everywhere else because it is global
-  console.log(name)
+// // example of local scope w/ parameters
+// function squareMe(number) {
+//   // The "number" parameter is variable that is LOCAL to the squareMe function
+//
+//   return number**2
+// }
+//
+// const result = squareMe(7)
+// console.log(result)
+//
+// /*
+// Now let's attempt to directly access the `number` parameter
+// */
+//
+// console.log(number) // <-- Error! The variable is local to the function can cannot be "directly" accessed outside of the function
+
+
+// more practice
+// declare a variable named 'actor' in the Global Scope
+const actor = "Daniel Craig"
+
+function bestBondActor() {
+  // declare a variable named 'actor' within a function (Local Scope)
+  const actor  = "Sean Connery"
+  return actor
 }
 
-bestGymnastEver() // logs 'Simone Biles'
-
-
-
-// example of local scope
-function showInspirationalMessage() {
-  // "message" variable is declared **inside** a function which means it is LOCAL to this function
-
-  const message = "Don't give up, you can do it!!"
-  console.log(message)
-}
-
-// prints message variable
-showInspirationalMessage()
+const result = bestBondActor()
+console.log(result) // <-- prints Sean Connery
 
 /*
-Now let's attempt to directly access the message variable
-and log it out to the console
+Now let's print out the `actor` variable
 */
 
-console.log(message) // <-- Error! The variable is local to the function can cannot be "directly" accessed outside of the function
+console.log(actor) // <-- prints Daniel Craig
