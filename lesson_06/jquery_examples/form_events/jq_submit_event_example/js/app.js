360ea555634ce433878f276f252b92a8fe570854
nhtaylor <nt334407@gmail.com>
2020-02-26 21:20:08 -0500

    rest of class work

--- lesson_06/jquery_examples/form_events/jq_submit_event_example/js/app.js
+++ lesson_06/jquery_examples/form_events/jq_submit_event_example/js/app.js
@@ -1,9 +1,13 @@
 $(function() {
+  // submit can only be used for .submit() methods
   $("#registration-form").submit((event) => {
+
     // call .preventDefault() to prevent form from being submitted
     event.preventDefault()
 
-    // use serialize or serialArray 
+    console.log("submitting form")
+
+    // use serialize or serialArray
     // to extract submitted information from the form
     console.log($(event.currentTarget).serializeArray())
     alert($(event.currentTarget).serialize())
